b98ad71792f473f5f69758977586243c5d209280
chris47368 <37160228+chris47368@users.noreply.github.com>
2021-11-26 05:43:42 +0000

    Add files via upload
    Fixed minor Diffusion/Mix function bug. This means that despite SLASH v4.7 encryption being nearly identical to SLASH versions v4.65 and earlier, encrypted data is no longer compatible between <v4.65 and v4.7.

---
+++ SLASH 128 bit V4.7/SLASH 128 bit/CFile.Designer.vb
@@ -0,0 +1,361 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CFile
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CFile))
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.CheckBox2)
+        Me.GroupBox3.Controls.Add(Me.RadioButton2)
+        Me.GroupBox3.Controls.Add(Me.Button6)
+        Me.GroupBox3.Controls.Add(Me.RadioButton1)
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Controls.Add(Me.CheckBox1)
+        Me.GroupBox3.Controls.Add(Me.Button2)
+        Me.GroupBox3.Controls.Add(Me.Button5)
+        Me.GroupBox3.Controls.Add(Me.TextBox2)
+        Me.GroupBox3.Controls.Add(Me.Label19)
+        Me.GroupBox3.Controls.Add(Me.Label7)
+        Me.GroupBox3.Controls.Add(Me.Label20)
+        Me.GroupBox3.Controls.Add(Me.Panel2)
+        Me.GroupBox3.Controls.Add(Me.TextBox8)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.Button3)
+        Me.GroupBox3.Controls.Add(Me.Label8)
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(464, 276)
+        Me.GroupBox3.TabIndex = 84
+        Me.GroupBox3.TabStop = False
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Location = New System.Drawing.Point(178, 167)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(79, 17)
+        Me.CheckBox2.TabIndex = 89
+        Me.CheckBox2.Text = "Auto select"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(370, 200)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(62, 17)
+        Me.RadioButton2.TabIndex = 88
+        Me.RadioButton2.Text = "Decrypt"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(6, 10)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(64, 21)
+        Me.Button6.TabIndex = 85
+        Me.Button6.Text = "<---"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(28, 200)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton1.TabIndex = 87
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Encrypt"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(9, 223)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(444, 31)
+        Me.Button1.TabIndex = 86
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(9, 167)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(111, 17)
+        Me.CheckBox1.TabIndex = 68
+        Me.CheckBox1.Text = "Replace input File"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(263, 171)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(173, 24)
+        Me.Button2.TabIndex = 85
+        Me.Button2.Text = "Clear All"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(264, 138)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(172, 26)
+        Me.Button5.TabIndex = 41
+        Me.Button5.Text = "Browse"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(62, 114)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(196, 20)
+        Me.TextBox2.TabIndex = 35
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(2, 144)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(58, 13)
+        Me.Label19.TabIndex = 40
+        Me.Label19.Text = "Output file:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(437, 52)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(13, 13)
+        Me.Label7.TabIndex = 82
+        Me.Label7.Text = "0"
+        '
+        'Label20
+        '
+        Me.Label20.AutoSize = True
+        Me.Label20.Location = New System.Drawing.Point(11, 114)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(50, 13)
+        Me.Label20.TabIndex = 36
+        Me.Label20.Text = "Input file:"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackgroundImage = CType(resources.GetObject("Panel2.BackgroundImage"), System.Drawing.Image)
+        Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel2.Location = New System.Drawing.Point(410, 52)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(22, 15)
+        Me.Panel2.TabIndex = 81
+        Me.Panel2.Tag = ""
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(62, 141)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(196, 20)
+        Me.TextBox8.TabIndex = 39
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 79
+        Me.Label1.Text = "Password:"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(264, 107)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(172, 26)
+        Me.Button3.TabIndex = 37
+        Me.Button3.Text = "Browse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.ForeColor = System.Drawing.Color.Red
+        Me.Label8.Location = New System.Drawing.Point(148, 81)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(161, 15)
+        Me.Label8.TabIndex = 80
+        Me.Label8.Text = "Password is Not secure."
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(62, 49)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(342, 20)
+        Me.TextBox3.TabIndex = 78
+        Me.TextBox3.UseSystemPasswordChar = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(327, 334)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(95, 13)
+        Me.Label4.TabIndex = 92
+        Me.Label4.Text = "Speed: 0 Blocks/s"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(234, 291)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 13)
+        Me.Label2.TabIndex = 90
+        Me.Label2.Text = "0%"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 334)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
+        Me.Label3.TabIndex = 91
+        Me.Label3.Text = "Status: None"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 307)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(464, 24)
+        Me.ProgressBar1.TabIndex = 89
+        '
+        'OpenFileDialog1
+        '
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.DefaultExt = "*"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        Me.Timer3.Interval = 1
+        '
+        'CFile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(488, 364)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "CFile"
+        Me.Text = "SLASH 128 Bit v4.7 - Encrypt a File"
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Button6 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Timer3 As Timer
+End Class
---
+++ SLASH 128 bit V4.7/SLASH 128 bit/Crypto_Func.vb
@@ -0,0 +1,85 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Threading
+Imports System.IO
+Module Crypto_Func
+    Function SHA256_gen(ByVal inputbytes As Byte()) As Byte()
+        Dim sha256 As SHA256 = SHA256Managed.Create()
+        'Dim bytes As Byte() = Encoding.UTF8.GetBytes(inputString)
+        Dim hash As Byte() = sha256.ComputeHash(inputbytes)
+        Dim stringBuilder As New StringBuilder()
+        Return hash
+    End Function
+    Function hextobyte(ByVal input As String)
+        Dim minp As String = input.Replace(" ", "")
+        minp = minp.Replace(vbCr, "").Replace(vbLf, "")
+        input.Trim()
+        Dim output((minp.Length / 2) - 1) As Byte
+        Dim a
+        Dim x As Integer = 1
+        While x < output.Length * 2
+            a = Mid(minp, x, 2)
+            output((x - 1) / 2) = Convert.ToByte(a, 16)
+            x += 2
+        End While
+        Return output
+    End Function
+    Function bytetohex(ByVal hex As Byte())
+        Dim stringBuilder As New StringBuilder()
+        For i As Integer = 0 To hex.Length - 1
+            stringBuilder.Append(hex(i).ToString("X2"))
+        Next
+        Return stringBuilder.ToString
+    End Function
+    Function contoint(ByVal input As String)
+        Dim output As Integer
+        If Integer.TryParse(input, output) Then
+            Return CInt(input)
+        Else
+            Dim inp As String = input
+            For i As Integer = 1 To input.Length
+                If Mid(inp, i, 1) = "." Then
+                    inp = Mid(inp, 1, i - 1)
+                    Return CInt(inp) + 1
+                End If
+            Next
+        End If
+        Return 0
+    End Function
+    Function idkey(ByVal key As Byte(), ByVal ident As String)
+        Dim pkey = key
+        Dim keyid As Byte() = Encoding.UTF8.GetBytes(ident)
+        For i As Integer = 0 To keyid.Length - 1
+            pkey(i) = pkey(i) Xor keyid(i)
+        Next
+        Return pkey
+    End Function
+    Function permutate_block(inputblock As Byte(), blocksize As Integer, pbox As Byte())
+        'Dim pbox As Byte() = hextobyte(perm_pbox)
+        'Dim inputblock As Byte() = hextobyte(TextBox1.Text)
+        Dim outputblock(inputblock.Length - 1) As Byte
+        'Dim blocksize As Integer = 64
+        Dim byte_count As Integer = blocksize / 8
+        'For i As Integer = 0 To 7 'byte level
+        '    For x As Integer = 0 To 7 'bit level
+
+        '    Next
+        'Next
+        Dim newbitloc = 0
+        Dim nx As Integer = 0
+        Dim ny As Integer = 0
+        Dim oldbitloc As Integer = 0
+        Dim ox As Integer = 0
+        Dim oy As Integer = 0
+        For i As Integer = 0 To blocksize - 1
+            newbitloc = pbox(i)
+            nx = newbitloc Mod 8 'new bit loc
+            ny = (newbitloc - nx) / byte_count ' new byte loc
+            oldbitloc = i
+            ox = oldbitloc Mod 8 'old byte loc
+            oy = (oldbitloc - ox) / byte_count 'old bit loc
+            outputblock(ny) = outputblock(ny) Or (((inputblock(ox) >> oy) And 1) << nx)
+        Next
+        Return outputblock
+    End Function
+End Module
